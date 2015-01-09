1a12b369352ae990d40110ff584842081d7b11fe
idriss92 <idriss2004@hotmail.com>
2015-01-09 10:55:52 +0100

    Fonctionnement partiel - Entree date pb

--- FlyAway/App_Start/BundleConfig.cs
+++ FlyAway/App_Start/BundleConfig.cs
@@ -54,7 +54,7 @@ namespace FlyAway
 
             // Définissez EnableOptimizations sur False pour le débogage. Pour plus d'informations,
             // visitez http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
