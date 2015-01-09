7ed7c37a7f007211a18cdef9558dcee4f7841963
idriss92 <idriss2004@hotmail.com>
2015-01-09 12:56:30 +0100

    Recherche Index implements Ok

--- FlyAway/Controllers/RechercheController.cs
+++ FlyAway/Controllers/RechercheController.cs
@@ -8,10 +8,37 @@ namespace FlyAway.Controllers
 {
     public class RechercheController : Controller
     {
+        FlyAwayDataEntities db = new FlyAwayDataEntities();
         // GET: Recherche
-        public ActionResult Index()
+        public ActionResult Index(string searchString, string search,string datedepart, string datearrivee, string heuredepart)
         {
-            return View();
+            var vol = from m in db.Vols select m;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                vol = vol.Where(s => s.Depart.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                vol = vol.Where(s => s.Arrivee.Contains(search));
+            }
+
+            if (!string.IsNullOrEmpty(datedepart))
+            {
+                vol = vol.Where(s => s.Arrivee.Contains(datedepart));
+            }
+
+            if (!string.IsNullOrEmpty(datearrivee))
+            {
+                vol = vol.Where(s => s.Arrivee.Contains(datearrivee));
+            }
+
+            if (!string.IsNullOrEmpty(heuredepart))
+            {
+                vol = vol.Where(s => s.Arrivee.Contains(heuredepart));
+            }
+            return View(vol);
+
         }
     }
 }
